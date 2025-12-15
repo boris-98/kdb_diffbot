aab4459fc577d0f6504afb4513fc852d8adaa364
boris-98 <radov.boris@gmail.com>
2025-12-15 19:11:41 +0100

    Cleaned launch_robot launch file. Added camera calibration file.

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -19,11 +19,7 @@ from launch.substitutions import Command
 
 def generate_launch_description():
 
-
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    package_name='kdb_diffbot' #<--- CHANGE ME
+    package_name='kdb_diffbot'
 
     robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
     controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controllers.yaml')
@@ -34,20 +30,6 @@ def generate_launch_description():
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
                 )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
     )
-
-    # joystick = IncludeLaunchDescription(
-    #             PythonLaunchDescriptionSource([os.path.join(
-    #                 get_package_share_directory(package_name),'launch','joystick.launch.py'
-    #             )]), launch_arguments={'use_sim_time': 'true'}.items()
-    # )
-
-    # twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
-    # twist_mux = Node(
-    #         package="twist_mux",
-    #         executable="twist_mux",
-    #         parameters=[twist_mux_params, {'use_sim_time': True}],
-    #         remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
-    #     )
 
     controller_manager = Node(
         package="controller_manager",
@@ -89,45 +71,6 @@ def generate_launch_description():
         )
     )
 
-    map_localization_node = Node(
-        package="kdb_diffbot",
-        executable="map_localization_node",
-        name="map_localization_node",
-        parameters=[{"use_sim_time": False}],
-        output="screen"
-    )
-
-    manual_map_publisher = Node(
-        package='kdb_diffbot',
-        executable='manual_map_publisher',
-        name='manual_map_publisher',
-        parameters=[{
-            'map_file': map_file_path,
-            'resolution': 0.02,
-            'origin_x': 0.0,
-            'origin_y': 0.0
-        }],
-        output='screen'
-    )
-    
-
-    # Code for delaying a node (I haven't tested how effective it is)
-    # 
-    # First add the below lines to imports
-    # from launch.actions import RegisterEventHandler
-    # from launch.event_handlers import OnProcessExit
-    #
-    # Then add the following below the current diff_drive_spawner
-    # delayed_diff_drive_spawner = RegisterEventHandler(
-    #     event_handler=OnProcessExit(
-    #         target_action=spawn_entity,
-    #         on_exit=[diff_drive_spawner],
-    #     )
-    # )
-    #
-    # Replace the diff_drive_spawner in the final return with delayed_diff_drive_spawner
-
-
 
     # Launch them all!
     return LaunchDescription([
@@ -136,7 +79,5 @@ def generate_launch_description():
         # twist_mux,
         delayed_controller_manager,
         delayed_diff_drive_spawner, # bili su obicni samo bez delayed
-        delayed_joint_broad_spawner,
-        manual_map_publisher,
-        map_localization_node
+        delayed_joint_broad_spawner
     ])
